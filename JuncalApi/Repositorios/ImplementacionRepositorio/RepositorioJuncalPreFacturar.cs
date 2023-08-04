aebbb84396fe36eb8d468492ecd014dbdce9262c
Damian Bosatta <daami21@gmail.com>
2023-08-04 15:01:19 -0300

    Arrglo repositorio pre facturar

--- JuncalApi/Repositorios/ImplementacionRepositorio/RepositorioJuncalPreFacturar.cs
+++ JuncalApi/Repositorios/ImplementacionRepositorio/RepositorioJuncalPreFacturar.cs
@@ -40,11 +40,19 @@ namespace JuncalApi.Repositorios.ImplementacionRepositorio
                                var remitosAgrupados = remitosOrdenados.GroupBy(r => new { r.IdAceria, r.IdContrato, r.IdMaterialRecibido })
                                                   .Select(group => new JuncalPreFacturar
                                                   {
+
                                                       IdAceria = group.Key.IdAceria,
                                                       IdContrato = group.Key.IdContrato,
                                                       IdMaterialRecibido = group.Key.IdMaterialRecibido,
-                                                      Peso = group.Sum(r => r.Peso)
-                                                  });
+                                                      Peso = group.Sum(r => r.Peso),                                                     
+                                                      IdOrden = group.Select(r => r.IdOrden).FirstOrDefault(),
+                                                      IdMaterialEnviado = group.Select(r => r.IdMaterialEnviado).FirstOrDefault(),
+                                                      PesoTara = group.Select(r => r.PesoTara).FirstOrDefault(),
+                                                      PesoBruto = group.Select(r => r.PesoBruto).FirstOrDefault(),
+                                                      PesoNeto = group.Select(r => r.PesoNeto).FirstOrDefault(),
+                                                      Remito = group.Select(r => r.Remito).FirstOrDefault(),                                                                                              
+                                                      Id = group.Select(r => r.Id).FirstOrDefault()
+                                                 });
 
                                return remitosAgrupados.ToList();
 
